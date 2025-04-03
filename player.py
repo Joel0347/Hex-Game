305d718dc64d2bd2db9013657492d54399e6d8b5
Joel0347 <127158086+Joel0347@users.noreply.github.com>
2025-04-03 15:00:46 -0400

    feat/ added alpha-betha cutting

--- player.py
+++ player.py
@@ -3,42 +3,73 @@ from player_def import Player
 from board import HexBoard
 
 class AI_Player(Player):
-
     def play(self, board):
         if (len(board.get_possible_moves()) == board.size ** 2):
             row = random.randint(0, board.size - 1)
             col = random.randint(0, board.size - 1)
             next_move = (row, col)
         else:
-            next_move, _ = self.minimax(board, True)
+            next_move, _ = self.minimax(board)
         return next_move
     
-    def minimax(self, board: HexBoard, level_parity=True):
-        opponent_id = 1 if self.player_id == 2 else 2
+    def minimax(
+        self, board: HexBoard, 
+        level_parity=True, 
+        alpha=-float('inf'), 
+        betha=float('inf')
+        ) -> tuple[tuple[int, int], int]:
         
-        if board.check_connection(opponent_id):
+        if board.check_connection(3 - self.player_id):
             return (-1, -1), 0
         elif board.check_connection(self.player_id):
             return (-1, -1), 1
         
         states = board.get_possible_moves()
-        states_values = [0 for _ in states]
+        next_move = states[0]
+        id = self.player_id if level_parity else 3 - self.player_id
         
-        if (level_parity):
-            id = self.player_id
-        else:
-            id = opponent_id
-        
-        for i in range(len(states)):
-            new_board = board.clone()
-            new_board.place_piece(states[i][0], states[i][1], id)
-            _, state_value = self.minimax(new_board, not level_parity)
-            states_values[i] = state_value
+        for st in states:
+            AI_Player.place_piece(board, st[0], st[1], id)
             
-        if (level_parity):
-            state_value = max(states_values)
-        else:
-            state_value = min(states_values)
+            if level_parity:
+                state_value = self.minimax(board, not level_parity, alpha=alpha)[1] 
+            else:
+                state_value = self.minimax(board, not level_parity, betha=betha)[1]
+
+            AI_Player.remove_piece(board, st[0], st[1], id)
             
-        return states[states_values.index(state_value)], state_value
+            if level_parity and alpha < state_value:
+                alpha = state_value
+                next_move = st
+            elif (not level_parity) and betha > state_value:
+                betha = state_value
+                next_move = st
+            
+            if betha <= alpha:
+                result = betha if level_parity else alpha
+                return (-1, -1), result
+            
+        result = alpha if level_parity else betha
         
+        return next_move, result
+    
+    
+    @staticmethod
+    def place_piece(board: HexBoard, row: int, col: int, player_id: int) -> bool:
+        """Coloca una ficha si la casilla está vacía."""
+        
+        if board.board[row][col] != 0:
+            return False
+        board.board[row][col] = player_id
+        board.player_positions[player_id].add((row,col))
+        return True
+        
+    @staticmethod 
+    def remove_piece(board: HexBoard, row: int, col: int, player_id: int) -> bool:
+        """Remueve una ficha del tablero."""
+        
+        if board.board[row][col] == 0:
+            return False
+        board.board[row][col] = 0
+        board.player_positions[player_id].remove((row,col))
+        return True
